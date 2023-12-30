e74239cc03f4ff1a3f4574f1d377b998a25f5fd9
luckysetiawan <luckysetiawan0802@gmail.com>
2023-12-30 19:09:15 +0700

    update links to const

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -2,7 +2,7 @@
     import { base } from '$app/paths'
     import "../app.css";
 
-    let links = [
+    const links = [
         { text: 'Home', href: base },
         { text: 'About', href: base + '/about' },
         { text: 'Other Tools', href: 'https://luckysetiawan.github.io/#/3' }
@@ -11,7 +11,7 @@
 
 <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
     <div class="flex flex-1">
-    <a href="{base}" class="text-2xl font-bold -m-1.5 p-1.5">BMI Calculator</a>
+    <a href="{links[0].href}" class="text-2xl font-bold -m-1.5 p-1.5">BMI Calculator</a>
     </div>
     <div class="flex gap-x-12">
     {#each links as {text, href}}
