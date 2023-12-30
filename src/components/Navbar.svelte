690a46394cfe2a9bb097ffcd3405d35e8f191b9e
luckysetiawan <luckysetiawan0802@gmail.com>
2023-12-30 18:45:00 +0700

    update base path

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,15 +1,17 @@
 <script>
+    import { base } from '$app/paths'
     import "../app.css";
+
     let links = [
-        { text: 'Home', href: '/' },
-        { text: 'About', href: '/about' },
+        { text: 'Home', href: base },
+        { text: 'About', href: base + '/about' },
         { text: 'Other Tools', href: 'https://luckysetiawan.github.io/#/3' }
     ];
 </script>
 
 <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
     <div class="flex flex-1">
-    <a href="/" class="text-2xl font-bold -m-1.5 p-1.5">BMI Calculator</a>
+    <a href="{base}" class="text-2xl font-bold -m-1.5 p-1.5">BMI Calculator</a>
     </div>
     <div class="flex gap-x-12">
     {#each links as {text, href}}
